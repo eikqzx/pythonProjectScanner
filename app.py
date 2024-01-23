a88d57c6ca76204ab2517dc315d8e78b6a211508
CapsuleCadaStral <142664046+CapsuleCadaStral@users.noreply.github.com>
2024-01-23 16:08:09 +0700

    Q

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def scan_document():
         dpi = request.json.get('dpi')
         pages = request.json.get('pages')
         scan_result = perform_scan(scanner_id,dpi,pages)
-
+        # print(scan_result)
         if scan_result:
             return jsonify({"message": "Scanning complete", "scannedImageBase64": scan_result})
         else:
@@ -69,12 +69,10 @@ def perform_scan(scanner_id,dpi,pages):
         if dev.DeviceID == scanner_id:
             dev.Items(1).Properties("Horizontal Resolution").Value = dpi
             dev.Items(1).Properties("Vertical Resolution").Value = dpi
-
-            if dev.Properties.Exists("Document Handling Select"):
-                # Set the document handling option to indicate the number of pages to scan
-                dev.Properties("Document Handling Select").Value = pages
-            else:
-                return ("The selected scanner does not support multiple pages.")
+            print("Document Handling Select",dev.Properties.Exists("Document Handling Select"))
+            # if dev.Properties.Exists("Document Handling Select"):
+            #     # Set the document handling option to indicate the number of pages to scan
+            #     dev.Properties("Document Handling Select").Value = pages
 
             image_format = '{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}'  # WIA_FORMAT_JPEG
             image_data = dev.Items(1).Transfer(image_format).FileData.BinaryData
@@ -88,8 +86,50 @@ def perform_scan(scanner_id,dpi,pages):
     finally:
         pythoncom.CoUninitialize()
 
-mode = "production"
+@app.route('/api/getProperties', methods=['POST'])
+def get_Handle():
+    try:
+        scanner_id = request.json.get('scannerId')
+        dpi = request.json.get('dpi')
+        pages = request.json.get('pages')
+        scan_result = getProperties(scanner_id,dpi,pages)
+        if scan_result:
+            return jsonify({"Properties": scan_result})
+        else:
+            return jsonify({"error": "Error occurred during scanning."})
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
+def getProperties(scanner_id,dpi,pages):
+    pythoncom.CoInitialize()  # Initialize COM
+    try:
+        wia = win32com.client.Dispatch("WIA.CommonDialog")
+        dev = wia.ShowSelectDevice()
+        if dev.DeviceID == scanner_id:
+            properties_info = []
+            for prop in dev.Properties:
+                property_info = {
+                    "Name": prop.Name,
+                    "PropertyID": prop.PropertyID,
+                    "Type": prop.Type,
+                    "Value": prop.Value,
+                }
+                properties_info.append(property_info)
+
+            return properties_info
+            # return dev.Properties.Exists("Document Handling Select")
+        else:
+            return None
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+    finally:
+        pythoncom.CoUninitialize()
+
+mode = "dev"
 host = "0.0.0.0"
+
 if __name__ == '__main__':
     if mode == "dev":
         app.run(host=host,port=5100, debug=True)
