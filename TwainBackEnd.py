500c7e35e79a65c02a43c113335c04dd8f855464
Attapon Sroysuwan <55727960+eikqzx@users.noreply.github.com>
2024-01-31 16:54:10 +0700

    Q

---
+++ TwainBackEnd.py
@@ -0,0 +1,118 @@
+from io import BytesIO
+import twain
+import PIL.ImageTk
+import PIL.Image
+import base64
+
+class ClassTwainBackEnd():
+    manager = None
+    source = None
+    resolution = 300
+    duplex = False
+
+    def matchMode(modeStr):
+        if modeStr == 'bw':
+            return twain.TWPT_BW
+        if modeStr == 'gray':
+            return twain.TWPT_GRAY
+        if modeStr == 'color':
+            return twain.TWPT_RGB
+
+    def scan(self,scannerName,postDpi,mode,isDuplex,removeBlank):
+        # sm = twain.SourceManager()
+        print("looping scan...")
+        imageList =[]
+        index = 1
+        boolDuplex = None
+        boolRemove = None
+
+        if isDuplex == 1:
+            boolDuplex = True
+        else:
+            boolDuplex = False
+
+        if removeBlank == 1:
+            boolRemove = True
+        else:
+            boolRemove = False
+
+            # this will show UI to allow user to select source
+        self.open(self,name=scannerName)
+        print("Open source...",self.source)
+        if self.source:
+            try:
+                self.source.set_capability(twain.ICAP_XRESOLUTION,twain.TWTY_FIX32, float(postDpi))
+                self.source.set_capability(twain.ICAP_YRESOLUTION,twain.TWTY_FIX32, float(postDpi))
+            except:
+                print("Could not set resolution to '%s'" % postDpi)
+                pass
+
+            try:
+                self.source.set_capability(twain.CAP_DUPLEXENABLED,twain.TWTY_BOOL, boolDuplex)
+            except:
+                print("Could not set duplex to '%s'" % isDuplex)
+                pass
+
+            try:
+                self.source.set_capability(twain.ICAP_PIXELTYPE,twain.TWTY_UINT16,  self.matchMode(mode))
+            except:
+                print("Could not set mode to '%s'" % isDuplex)
+                pass
+
+            try:
+                self.source.set_capability(twain.ICAP_AUTODISCARDBLANKPAGES,twain.TWTY_BOOL, boolRemove)
+            except:
+                print("Could not set mode to '%s'" % removeBlank)
+                pass
+
+            try:
+                self.source.request_acquire(0,0)
+            except:
+                print("AcquisitionError")
+                pass
+            while self.next(self):
+                image = self.capture(self)
+                imageList.append(({"imageIndex":index,"base64Image":base64.b64encode(image.getvalue()).decode("utf-8") }))
+                index += 1
+            return imageList
+        else:
+            print("User clicked cancel")
+
+    def scannerList():
+        sourceList = twain.SourceManager().GetSourceList()
+        return sourceList
+
+    def open(self, name):
+        self.manager = twain.SourceManager(0,ProductName=name )
+        if not self.manager:
+            return
+        if self.source:
+            self.source.destroy()
+            self.source=None
+        self.source = self.manager.OpenSource(name)
+        if self.source:
+            print("%s: %s" % ( name, self.source.GetSourceName() ))
+
+    def next(self):
+        try:
+            self.source.GetImageInfo()
+            return True
+        except:
+            return False
+    def capture(self):
+            fileName = "tmp.tmp"
+            try:
+                (handle, more_to_come) = self.source.XferImageNatively()
+            except:
+                return None
+
+            bmp_bytes = twain.dib_to_bm_file(handle)
+            img = PIL.Image.open(BytesIO(bmp_bytes), formats=["bmp"])
+            img_buffer = BytesIO()
+            img.save(img_buffer, format="BMP")
+            twain.global_handle_free(handle)
+            return img_buffer
+
+    def close(self):
+        del self.manager
+
--- test.py
+++ test.py
@@ -1,16 +1,7 @@
-from io import BytesIO
-import twain
-import tkinter
-from tkinter import ttk
-import logging
-import PIL.ImageTk
-import PIL.Image
-import base64
 from waitress import serve
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
-import imghdr
-
+from TwainBackEnd import ClassTwainBackEnd
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -19,57 +10,20 @@ CORS(app)  # Enable CORS for all routes
 def indexx():
     scannerName = request.json.get('scannerName')
     postDpi = request.json.get('postDpi')
-    numPage = request.json.get('numPage')
-    modeStr = request.json.get('mode')
-    list = scan(scannerName,postDpi,modeStr,numPage)
+    modeStr = request.json.get('mode') # ‘bw’, ‘gray’, ‘color’
+    isDuplex = request.json.get('isDuplex')
+    removeBlank = request.json.get('removeBlank')
+    list = ClassTwainBackEnd.scan(self=ClassTwainBackEnd,scannerName=scannerName,postDpi=postDpi,mode=modeStr,isDuplex=isDuplex,removeBlank=removeBlank)
     # print("list:",list)
+    if list == None:
+        list = "Scanner is Offline"
     return jsonify({"ImageList":list})
-
-def matchMode(modeStr):
-    if modeStr == 'bw':
-        return twain.TWPT_BW
-    if modeStr == 'gray':
-        return twain.TWPT_GRAY
-    if modeStr == 'color':
-        return twain.TWPT_RGB
-
-def scan(scannerName,postDpi,mode,numPage):
-    imageList =[]
-    index = 1
-    with twain.SourceManager() as sm:
-        # this will show UI to allow user to select source
-        src = sm.open_source(scannerName)
-        if src:
-            src.set_capability(twain.ICAP_XRESOLUTION,twain.TWTY_FIX32, float(postDpi))
-            src.set_capability(twain.ICAP_YRESOLUTION,twain.TWTY_FIX32, float(postDpi))
-            src.set_capability(twain.CAP_FEEDERENABLED, twain.TWTY_FIX32, float(postDpi))
-            #  can be ‘bw’, ‘gray’, ‘color’
-            src.set_capability(twain.ICAP_PIXELTYPE,twain.TWTY_UINT16,  matchMode(mode))
-
-            src.request_acquire(show_ui=False, modal_ui=False)
-            (handle, remaining_count) = src.xfer_image_natively()
-
-            # while index <= numPage:
-            bmp_bytes = twain.dib_to_bm_file(handle)
-            img = PIL.Image.open(BytesIO(bmp_bytes), formats=["bmp"])
-            img_buffer = BytesIO()
-            img.save(img_buffer, format="BMP")
-            imageList.append(({"imageIndex":remaining_count,"base64Image":base64.b64encode(img_buffer.getvalue()).decode("utf-8") }))
-            # index += 1
-
-            return imageList
-        else:
-            print("User clicked cancel")
 
 @app.route("/getList",methods=['GET'])
 def getList():
-    list = scannerList()
+    list = ClassTwainBackEnd.scannerList()
     # print("list:",list)
     return jsonify({"scannerList":list})
-
-def scannerList():
-    sourceList = twain.SourceManager().GetSourceList()
-    return sourceList
 
 mode = "dev"
 host = "0.0.0.0"
@@ -80,10 +34,3 @@ if __name__ == '__main__':
     else:
         print("Flask API is running at "+host)
         serve(app,host=host,port=5100)
-
-# logging.basicConfig(level=logging.DEBUG)
-# root = tkinter.Tk()
-# frm = ttk.Frame(root, padding=10)
-# frm.grid()
-# ttk.Button(frm, text="Scan", command=scan).grid(column=0, row=0)
-# root.mainloop()
